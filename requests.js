d2599214a9622976920a5d96f4ed7ff2a3aca26a
logomonicLearning <logomoniclearning@gmail.com>
2018-09-06 12:25:52 +0100

    added document ready

--- requests.js
+++ requests.js
@@ -1,23 +1,22 @@
-var demo=$('#demo');
-$('#myBtn').click(function(){
-	var XHR=new XMLHttpRequest();
-	XHR.open('GET', 'https://github.com/logomonicLearning/AjaxPractice/onePiece.json', true);
-	XHR.onload=function(){
-		var data=JSON.parse(XHR.responseText);
-		renderHTML(data);
-	};
-	XHR.send();
-});
+$(document).ready(function(){
+	var demo=$('#demo');
+	$('#myBtn').click(function(){
+		var XHR=new XMLHttpRequest();
+		XHR.open('GET', 'https://github.com/logomonicLearning/AjaxPractice/onePiece.json', true);
+		XHR.onload=function(){
+			var data=JSON.parse(XHR.responseText);
+			renderHTML(data);
+		};
+		XHR.send();
+	});
 
-function renderHTML(data){
-	var myStr=""
-	
-	for(i=0;i<data.length; i++){
-		myStr+="<p>"+data[i]+"</p>"
+	function renderHTML(data){
+		var myStr=""
+		
+		for(i=0;i<data.length; i++){
+			myStr+="<p>"+data[i]+"</p>"
+		}
+
+		demo.insertAdjacentHTML('beforeend', myStr);	
 	}
-
-
-	demo.insertAdjacentHTML('beforeend', myStr);	
-
-}
-
+});
